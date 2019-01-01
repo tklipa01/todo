319a74a1bf08c0856c2e83db8975f0002b4a9377
Travis Klipa <travis.klipa@gmail.com>
2019-01-01 15:11:09 -0500

    removing files

--- dist/config.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.config = {
-    development: {
-        mongoUrl: "mongodb+srv://admin:mongoadmin@cluster0-kzgvq.gcp.mongodb.net/admin",
-        database: "Todo"
-    }
-};
-//# sourceMappingURL=config.js.map
